d0a0cd58f2bd2cebafd2b97bf0f62652644739c8
Richard Cook <blubparadox@gmail.com>
2019-11-13 22:24:56 -0500

    Renaming some variables. Filtering the directory file list to only have .json files.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ const program = require("commander");
 program
   .version('0.0.1')
   .description('A CLI for making couchdb files from synthea.')
-  .option('-d --directory <directory>', 'Directory where the files are', '/home/rrc/gitcode/synthea/output/fhir')
-  // .option('-d --directory <directory>', 'Directory where the files are', '.')
+  // .option('-d --directory <directory>', 'Directory where the files are', '/home/rrc/gitcode/synthea/output/fhir')
+  .option('-d --directory <directory>', 'Directory where the files are', '.')
   .parse(process.argv);
 
   if (!process.argv.slice(2).length) {
@@ -23,11 +23,11 @@ program
   console.log(`hello world from ${program.description()} at ${process.cwd()}`);
   console.log(`The directory we\'re using is at ${program.directory}`);
 
-  const dir = fs.readdirSync(program.directory, {withFileTypes: true});
+  const filelist = fs.readdirSync(program.directory).filter((d) => d.endsWith('.json'));
 
-  for (const dirent of dir) {
-    console.log(dirent.name);
-    const data = fs.readFileSync(program.directory + '/' + dirent.name, {encoding: 'utf8'});
+  for (const filename of filelist) {
+    console.log(filename);
+    const data = fs.readFileSync(program.directory + '/' + filename, {encoding: 'utf8'});
     const jdata = JSON.parse(data);
     console.log(jdata.entry[100].resource.id);
 
