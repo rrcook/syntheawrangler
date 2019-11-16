34a1dec6a01f6e7d2ee4e73fd9ad77f744a98ea5
Richard Cook <blubparadox@gmail.com>
2019-11-15 23:27:02 -0500

    First happy path that makes a file that can be bulk imported into couchdb. Improvements to come.

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ program
   .description('A CLI for making couchdb files from synthea.')
   // .option('-d --directory <directory>', 'Directory where the files are', '/home/rrc/gitcode/synthea/output/fhir')
   .option('-d --directory <directory>', 'Directory where the files are', '.')
+  .option('-o --outputfile <outputfile>', 'Output file', 'patients.json')
   .parse(process.argv);
 
   if (!process.argv.slice(2).length) {
@@ -23,12 +24,29 @@ program
   console.log(`hello world from ${program.description()} at ${process.cwd()}`);
   console.log(`The directory we\'re using is at ${program.directory}`);
 
-  const filelist = fs.readdirSync(program.directory).filter((d) => d.endsWith('.json'));
+  // on MacOS, there can be a .DS_Store file. Might be others.
+  const fileList = fs.readdirSync(program.directory).filter((d) => d.endsWith('.json'));
 
-  for (const filename of filelist) {
-    console.log(filename);
-    const data = fs.readFileSync(program.directory + '/' + filename, {encoding: 'utf8'});
+  // Object to build to write out.
+  const outputObject:any = {};
+
+  // "Global" properties of the object.
+  // All FHIR objects will be put in here.
+  outputObject.docs = [];
+
+  fileList.forEach((fileName, i) => {
+    console.log(fileName);
+    const data = fs.readFileSync(program.directory + '/' + fileName, {encoding: 'utf8'});
     const jdata = JSON.parse(data);
-    console.log(jdata.entry[100].resource.id);
 
-  }
+    // Add couchdb related properties to the original object
+    jdata._id = 'id' + (i + 1);
+
+    outputObject.docs.push(jdata);
+  
+  });
+
+  console.log("breakpoint");
+  const outputText = JSON.stringify(outputObject, null, 2);
+
+  fs.writeFileSync(program.directory + '/' + program.outputfile, outputText, {encoding: 'utf8'});
